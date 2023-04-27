cc295bb622028ae72b25b958b4bd5ea689191ea1
builde7b0b <ggives3@gmail.com>
2023-04-27 15:00:17 -0700

    First Commit, Init Springboot Project

---
+++ src/test/java/com/builderAPI/builder/BuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.builderAPI.builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
