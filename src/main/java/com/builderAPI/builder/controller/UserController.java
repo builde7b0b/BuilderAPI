03abd604c250dd48b30d71109f846b4d7c459aa4
builde7b0b <ggives3@gmail.com>
2023-04-28 09:56:32 -0700

    add test endpoint helloworld

--- src/main/java/com/builderAPI/builder/controller/UserController.java
+++ src/main/java/com/builderAPI/builder/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.builderAPI.builder.controller;
 
+import com.builderAPI.builder.model.User;
 import com.builderAPI.builder.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -14,9 +19,24 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+
+    @GetMapping(path = "/hello-world/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/")
     public List<com.builderAPI.builder.model.User> getAllUsers() {
         return userService.getAllUsers();
     }
 
-    
+    @GetMapping("/")
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable(value = "id") Long userId) {
+        Optional<User> user = userService.getUserById(userId);
+        return ResponseEntity.ok().body(user);
+    }
+
+
+
+
 }
