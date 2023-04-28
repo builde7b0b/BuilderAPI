7ccb9d1789620ba175cfc713783c5f36fd0edb6b
builde7b0b <ggives3@gmail.com>
2023-04-28 06:45:53 -0700

    Add roles field to User model, craft getters and setters

--- src/main/java/com/builderAPI/builder/Model/User.java
+++ src/main/java/com/builderAPI/builder/Model/User.java
@@ -12,5 +12,68 @@ public class User {
     private String emailAddress;
     private String password;
 
+    private String roles;
 
+
+    public User() {
+    }
+
+    public User(Long id, String userName, String emailAddress, String password, String roles) {
+        this.id = id;
+        this.userName = userName;
+        this.emailAddress = emailAddress;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", password='" + password + '\'' +
+                ", roles='" + roles + '\'' +
+                '}';
+    }
 }
