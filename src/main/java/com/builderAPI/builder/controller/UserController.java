52aa080117bcb911f869733a70d764e44ee38479
builde7b0b <ggives3@gmail.com>
2023-04-28 08:35:58 -0700

    create service package, userService Class

--- src/main/java/com/builderAPI/builder/controller/UserController.java
+++ src/main/java/com/builderAPI/builder/controller/UserController.java
@@ -1,4 +1,10 @@
 package com.builderAPI.builder.controller;
 
 public class UserController {
+
+    private UserService userService;
+
+    public List<com.builderAPI.builder.Model.User> getAllUsers() {
+        return userService.getAllUsers();
+    }
 }
---
+++ src/main/java/com/builderAPI/builder/services/UserService.java
@@ -0,0 +1,4 @@
+package com.builderAPI.builder.services;
+
+public class UserService {
+}
