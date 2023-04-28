52f6ca0b316ef1a78abc1693dcf9d04d202fb939
builde7b0b <ggives3@gmail.com>
2023-04-28 07:38:04 -0700

    Update based on Instructor Input / Guidance

---
+++ src/test/java/BuilderTest.java
@@ -0,0 +1,22 @@
+import com.builderAPI.builder.Model.User;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class BuilderTest {
+
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setUserName("John Doe");
+        user.setEmailAddress("john.doe@example.com");
+        user.setPassword("password");
+        user.setRoles("ROLE_USER");
+        assertNotNull(user.getId());
+        assertEquals("John Doe", user.getUserName());
+        assertEquals("john.doe@example.com", user.getEmailAddress());
+        assertEquals("password", user.getPassword());
+        assertEquals("ROLE_USER", user.getRoles());
+    }
+}
