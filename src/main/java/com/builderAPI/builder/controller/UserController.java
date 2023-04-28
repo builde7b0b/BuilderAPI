cc2ecfde219f71f6f12f7e93990460e872f934a9
builde7b0b <ggives3@gmail.com>
2023-04-28 09:39:36 -0700

    implement UserController validation

--- src/main/java/com/builderAPI/builder/controller/UserController.java
+++ src/main/java/com/builderAPI/builder/controller/UserController.java
@@ -1,15 +1,22 @@
 package com.builderAPI.builder.controller;
 
 import com.builderAPI.builder.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
     public List<com.builderAPI.builder.model.User> getAllUsers() {
         return userService.getAllUsers();
     }
+
+    
 }
