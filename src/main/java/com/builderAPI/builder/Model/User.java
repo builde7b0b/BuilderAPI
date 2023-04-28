7e2bf57df2a9868c7cfd574bbe1f041c572a72cc
builde7b0b <ggives3@gmail.com>
2023-04-28 08:14:30 -0700

    update docstring - User model

--- src/main/java/com/builderAPI/builder/Model/User.java
+++ src/main/java/com/builderAPI/builder/Model/User.java
@@ -25,10 +25,20 @@ public class User {
     @Column
     private String roles;
 
-
+    /**
+     * Default constructor for User class
+     */
     public User() {
     }
 
+    /**
+     * Constructor with Parameters for User class
+     * @param id the user's ID #
+     * @param userName the user's name
+     * @param emailAddress the user's email
+     * @param password the user's password
+     * @param roles the user's roles
+     */
     public User(Long id, String userName, String emailAddress, String password, String roles) {
         this.id = id;
         this.userName = userName;
@@ -37,46 +47,96 @@ public class User {
         this.roles = roles;
     }
 
+    /**
+     * Getter Method for user ID
+     * @return ID for the user
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * setter method for user ID
+     * @param id the ID to set for user
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * getter method for Name of User
+     * @return the name of user
+     */
     public String getUserName() {
         return userName;
     }
+
+    /**
+     * Setter method for use rname
+     * @param userName the name set for the user
+     */
 
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    /**
+     * getter method for email address
+     * @return the email address of the user
+     */
     public String getEmailAddress() {
         return emailAddress;
     }
+
+    /**
+     * setter method for user email address
+     * @param emailAddress the email address to set for the user
+     */
 
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
 
+    /**
+     * getter method for use rpassword
+     * @return the password for the user
+     */
+
     public String getPassword() {
         return password;
     }
+
+    /**
+     * setter method for password
+     * @param password the password set for the user
+     */
 
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * getter method for user roled
+     * @return roles the roles to set for user
+     */
     public String getRoles() {
         return roles;
     }
+
+    /**
+     * setter method for user roles
+     * @param roles the roles to set for the user
+     */
 
     public void setRoles(String roles) {
         this.roles = roles;
     }
 
+    /**
+     * Override of toString method to return user information as a string
+     *
+     * @return a string representation of the user info
+     */
     @Override
     public String toString() {
         return "User{" +
