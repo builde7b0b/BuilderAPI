672fd3bc811fcef088e92481040bc91f6faa01b5
builde7b0b <ggives3@gmail.com>
2023-04-28 21:16:57 -0700

    create RoleName java class

--- src/main/java/com/builderAPI/builder/Model/Role.java
+++ src/main/java/com/builderAPI/builder/Model/Role.java
@@ -1,14 +1,16 @@
 package com.builderAPI.builder.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
     private RoleName name;
 
     //constructors, getters and setters
 }
 
-public enum RoleName {
-    ROLE_USER,
-    ROLE_ADMIN
-}
---
+++ src/main/java/com/builderAPI/builder/Model/RoleName.java
@@ -0,0 +1,6 @@
+package com.builderAPI.builder.model;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
