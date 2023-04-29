a45e5bb3165b30d72f8353682fc618779e3b1ab8
builde7b0b <ggives3@gmail.com>
2023-04-28 21:14:05 -0700

    setup body Role class

--- src/main/java/com/builderAPI/builder/Model/Role.java
+++ src/main/java/com/builderAPI/builder/Model/Role.java
@@ -1,4 +1,14 @@
 package com.builderAPI.builder.model;
 
 public class Role {
+    private Long id;
+
+    private RoleName name;
+
+    //constructors, getters and setters
+}
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
 }
