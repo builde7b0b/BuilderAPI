dd25fc6e7760a41aa5df715a9a308f65d0ac044c
builde7b0b <ggives3@gmail.com>
2023-04-28 20:13:01 -0700

    add docstrings to UserService

--- src/main/java/com/builderAPI/builder/services/UserService.java
+++ src/main/java/com/builderAPI/builder/services/UserService.java
@@ -16,18 +16,41 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    /**
+     * Returns a list of all users in the system.
+     * @return List of all users
+     */
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
+    /**
+     * Retrives a user by their ID
+     *
+     * @param userId The ID of the user to retrieve.
+     * @return Optional containing the user if found, empty otherwise
+     */
     public Optional<User> getUserById(Long userId){
         return userRepository.findById(userId);
     }
 
+    /**
+     * Create a new user
+     * @param user The user to create
+     * @return The Created user
+     */
     public User createUser(User user) {
         return userRepository.save(user);
     }
 
+    /**
+     * Updates an existing user.
+     *
+     * @param userId The ID of the user to update.
+     * @param userDetails The details to update.
+     * @return The updated user.
+     * @throws ChangeSetPersister.NotFoundException If the user is not found.
+     */
     public User updateUser(Long userId, User userDetails) throws ChangeSetPersister.NotFoundException {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new ChangeSetPersister.NotFoundException());
@@ -40,6 +63,13 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    /**
+     * Deletes an existing user
+     *
+     * @param userId The ID of the user to delete
+     * @throws ConfigDataResourceNotFoundException If the user is not found.
+     * @throws ChangeSetPersister.NotFoundException If the user is not found.
+     */
     public void deleteUser(Long userId) throws ConfigDataResourceNotFoundException, ChangeSetPersister.NotFoundException {
         User user = userRepository.findById(userId).orElseThrow(() -> new ChangeSetPersister.NotFoundException());
     }
