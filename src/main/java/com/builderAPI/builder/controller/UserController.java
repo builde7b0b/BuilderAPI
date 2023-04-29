4697a22e2143704a96125e6900be7c2068dd6e87
builde7b0b <ggives3@gmail.com>
2023-04-28 20:17:42 -0700

    add docstrings to UserController

--- src/main/java/com/builderAPI/builder/controller/UserController.java
+++ src/main/java/com/builderAPI/builder/controller/UserController.java
@@ -19,17 +19,32 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
+    /**
+     * Returns a "Hello World!" message.
+     *
+     * @return A "Hello world!" message.
+     */
     @GetMapping(path = "/hello-world/")
     public String helloWorld() {
         return "Hello World!";
     }
 
+    /**
+     * Returns a list of all users in the system.
+     *
+     * @return List of all users.
+     */
     @GetMapping("/")
     public List<com.builderAPI.builder.model.User> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    /**
+     * Retrieves a user by their ID
+     *
+     * @param userId The ID of the user to retrieve.
+     * @return ResponseEntity containing the user if found, 404 Not found otherwise
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Optional<User>> getUserById(@PathVariable(value = "id") Long userId) {
         Optional<User> user = userService.getUserById(userId);
--- src/main/java/com/builderAPI/builder/repository/UserRepository.java
+++ src/main/java/com/builderAPI/builder/repository/UserRepository.java
@@ -3,6 +3,11 @@ package com.builderAPI.builder.repository;
 import com.builderAPI.builder.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for managing user data.
+ *
+ *
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
