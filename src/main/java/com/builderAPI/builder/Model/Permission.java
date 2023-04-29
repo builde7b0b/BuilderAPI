99f28003eb9dcb07f6cfa81a5a951b365f2daf26
builde7b0b <ggives3@gmail.com>
2023-04-28 21:20:17 -0700

    define roles for User Model

--- src/main/java/com/builderAPI/builder/Model/Permission.java
+++ src/main/java/com/builderAPI/builder/Model/Permission.java
@@ -1,4 +1,38 @@
 package com.builderAPI.builder.model;
 
+/**
+ * a Permission class that represents a permission in your application
+ *
+ */
 public class Permission {
+
+    private Long id;
+    private Permission name;
+
+    public Permission() {
+    }
+
+    public Permission(Long id, Permission name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    //constructors, getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Permission getName() {
+        return name;
+    }
+
+    public void setName(Permission name) {
+        this.name = name;
+    }
+
+
 }
--- src/main/java/com/builderAPI/builder/Model/RoleName.java
+++ src/main/java/com/builderAPI/builder/Model/RoleName.java
@@ -1,5 +1,9 @@
 package com.builderAPI.builder.model;
 
+/**
+ * enum class that defines the available permissions
+ *
+ */
 public enum RoleName {
     ROLE_USER,
     ROLE_ADMIN
