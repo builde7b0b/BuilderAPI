a9dfdf5c3602f386bb951b23683accf4f26dc68c
builde7b0b <ggives3@gmail.com>
2023-04-30 06:36:10 -0700

    add method to login

--- src/main/java/com/builderAPI/builder/security/SecurityController.java
+++ src/main/java/com/builderAPI/builder/security/SecurityController.java
@@ -1,7 +1,12 @@
 package com.builderAPI.builder.security;
 
 import com.builderAPI.builder.services.UserService;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +17,18 @@ public class SecurityController {
     private JwtTokenUtil jwtTokenUtil;
 
     private UserService userService;
+
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+        final UserDetails userDetails = userService.loadUserByUsername(loginRequest.getEmail());
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new LoginResponse(token));
+
+
+
+    }
+    
 
 
 }
