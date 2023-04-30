d35a58cd0cbd42df3ec109692ec99e747a6b6808
builde7b0b <ggives3@gmail.com>
2023-04-30 06:27:44 -0700

    add variables to SecurityController

--- src/main/java/com/builderAPI/builder/security/SecurityController.java
+++ src/main/java/com/builderAPI/builder/security/SecurityController.java
@@ -1,7 +1,17 @@
 package com.builderAPI.builder.security;
 
+import com.builderAPI.builder.services.UserService;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecurityController {
+
+    private AuthenticationManager authenticationManager;
+
+    private JwtTokenUtil jwtTokenUtil;
+
+    private UserService userService; 
+
+
 }
