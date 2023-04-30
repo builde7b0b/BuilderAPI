48e86f90b694f4004f20e5507a044e9fa6324f34
builde7b0b <ggives3@gmail.com>
2023-04-30 07:20:15 -0700

    implement and add dependencies

--- src/main/java/com/builderAPI/builder/security/InvalidJwtToken.java
+++ src/main/java/com/builderAPI/builder/security/InvalidJwtToken.java
@@ -1,4 +1,18 @@
 package com.builderAPI.builder.security;
 
+import java.time.LocalDateTime;
+
 public class InvalidJwtToken {
+
+    private Long id;
+
+    private String token;
+
+    private LocalDateTime invalidatedAt;
+
+
+
+    //constructors, getters and setters
+
+
 }
