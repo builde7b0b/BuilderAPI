1f1bfd2dccc4729300d908e930f8af05e11fadef
builde7b0b <ggives3@gmail.com>
2023-04-30 06:49:23 -0700

    add jwt secret

--- src/main/java/com/builderAPI/builder/security/JwtTokenUtil.java
+++ src/main/java/com/builderAPI/builder/security/JwtTokenUtil.java
@@ -1,4 +1,16 @@
 package com.builderAPI.builder.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JwtTokenUtil {
+
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+
+    
 }
