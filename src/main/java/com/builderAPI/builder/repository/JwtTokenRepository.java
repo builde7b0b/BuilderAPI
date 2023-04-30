37aa5fae655042cb5906610454d1fbd008c6e5d1
builde7b0b <ggives3@gmail.com>
2023-04-30 07:23:43 -0700

    validate class

--- src/main/java/com/builderAPI/builder/repository/JwtTokenRepository.java
+++ src/main/java/com/builderAPI/builder/repository/JwtTokenRepository.java
@@ -1,6 +1,10 @@
 package com.builderAPI.builder.repository;
 
+import com.builderAPI.builder.security.InvalidJwtToken;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface JwtTokenRepository extends JpaRepository<InvalidJwtToken, Long> {
--- src/main/java/com/builderAPI/builder/security/InvalidJwtToken.java
+++ src/main/java/com/builderAPI/builder/security/InvalidJwtToken.java
@@ -1,15 +1,55 @@
 package com.builderAPI.builder.security;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "invalid_jwt_token")
 public class InvalidJwtToken {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "token")
     private String token;
 
+    @Column(name = "invalidated_at")
     private LocalDateTime invalidatedAt;
 
+    public InvalidJwtToken() {
+    }
+
+    public InvalidJwtToken(Long id, String token, LocalDateTime invalidatedAt) {
+        this.id = id;
+        this.token = token;
+        this.invalidatedAt = invalidatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public LocalDateTime getInvalidatedAt() {
+        return invalidatedAt;
+    }
+
+    public void setInvalidatedAt(LocalDateTime invalidatedAt) {
+        this.invalidatedAt = invalidatedAt;
+    }
 
 
     //constructors, getters and setters
