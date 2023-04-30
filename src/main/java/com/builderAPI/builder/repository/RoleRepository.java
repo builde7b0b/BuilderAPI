0e278c2c8b065c53a326d80d5fff192bd8fab15f
builde7b0b <ggives3@gmail.com>
2023-04-30 05:45:37 -0700

    add save method to RoleRepo

--- src/main/java/com/builderAPI/builder/repository/RoleRepository.java
+++ src/main/java/com/builderAPI/builder/repository/RoleRepository.java
@@ -1,7 +1,9 @@
 package com.builderAPI.builder.repository;
 
+import com.builderAPI.builder.model.Role;
 import com.builderAPI.builder.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<User, Long> {
+    Role save(Role role);
 }
