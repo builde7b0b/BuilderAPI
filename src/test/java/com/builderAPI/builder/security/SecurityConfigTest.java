f63c5cb48d19273dfe0c60d9ede3473a647f4695
builde7b0b <ggives3@gmail.com>
2023-04-30 06:11:13 -0700

    start annotations for SecurityConfigTest

--- src/test/java/com/builderAPI/builder/security/SecurityConfigTest.java
+++ src/test/java/com/builderAPI/builder/security/SecurityConfigTest.java
@@ -1,4 +1,12 @@
 package com.builderAPI.builder.security;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class SecurityConfigTest {
 }
