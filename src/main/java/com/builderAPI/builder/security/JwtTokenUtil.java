b210110c0fb998c92cac7504f143898e67b32bbc
builde7b0b <ggives3@gmail.com>
2023-04-30 06:55:54 -0700

    partially implement class, add methods/vars

--- src/main/java/com/builderAPI/builder/security/JwtTokenUtil.java
+++ src/main/java/com/builderAPI/builder/security/JwtTokenUtil.java
@@ -2,6 +2,17 @@ package com.builderAPI.builder.security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtTokenUtil {
@@ -11,6 +22,34 @@ public class JwtTokenUtil {
     @Value("${jwt.secret}")
     private String secret;
 
+    //retrieve username from jwt token
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
 
-    
+    //Retrieve expiration date from jwt token
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    //check if the token has expired
+
+
+    //generate token for user
+
+
+    // while creating the token
+    // do this
+
+
+
 }
