6ec9c2004cbb9d41abb3de8c4cf58e24696ae2c4
builde7b0b <ggives3@gmail.com>
2023-04-30 05:15:05 -0700

    implement UserRole class

--- src/main/java/com/builderAPI/builder/Model/UserRole.java
+++ src/main/java/com/builderAPI/builder/Model/UserRole.java
@@ -1,4 +1,52 @@
 package com.builderAPI.builder.model;
 
 public class UserRole {
+
+    private Long id;
+
+    private User user;
+
+    private Role role;
+
+    public UserRole() {
+    }
+
+    public UserRole(Long id, User user, Role role) {
+        this.id = id;
+        this.user = user;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole{" +
+                "id=" + id +
+                ", user=" + user +
+                ", role=" + role +
+                '}';
+    }
 }
