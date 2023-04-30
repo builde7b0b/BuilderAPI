22d8677cb10b33923f5d3beb38f3ab7eb4734acd
builde7b0b <ggives3@gmail.com>
2023-04-30 05:17:25 -0700

    add validation to UserRole

--- src/main/java/com/builderAPI/builder/Model/UserRole.java
+++ src/main/java/com/builderAPI/builder/Model/UserRole.java
@@ -1,10 +1,20 @@
 package com.builderAPI.builder.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_roles")
 public class UserRole {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
 
     private Role role;
 
